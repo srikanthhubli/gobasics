f3aecc0a2d9b7e9297d12ddff5863251d2023fbd
srikanthhubli <srikanthhubli@hotmail.com>
2021-05-29 20:11:14 +0530

    add file variablesandconstants

---
+++ variablesandconstants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//declaring a variable
+	var num int = 2
+	var num2 = 3
+	var result = num + num2
+
+	//:= takes care of assignment
+	name := "Srikanth"
+
+	fmt.Println(result)
+	fmt.Println(name)
+}
